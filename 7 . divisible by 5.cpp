562204dc34dd9c0716ad8f5e174faaccf011b64a
venkuavula2005 <133081626+venkuavula2005@users.noreply.github.com>
2023-09-11 15:01:22 +0530

    Add files via upload

---
+++ 7 . divisible by 5.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter the number:");
+	scanf("%d",&n);
+	if(n%5==0)
+	printf("it is divisible");
+	else
+	printf("it is not divisible");
+	return 0;
+}
+
