8a783f31f258d155f2e513608f89bd7eeccaefbc
venkuavula2005 <133081626+venkuavula2005@users.noreply.github.com>
2023-09-11 15:01:50 +0530

    Add files via upload

---
+++ 8 . equal or not.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int n1,n2;
+	printf("enter the number:");
+	scanf("%d%d",&n1,&n2);
+	if(n1==n2)
+	printf("it is equal");
+	else
+	printf("it is not equal");
+	return 0;
+}
